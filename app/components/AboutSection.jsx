65ba032d3cbdc20db455c7ca278942c51e24ee43
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-08-07 15:08:36 +0530

    Updates for new branch

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -84,7 +84,7 @@ const AboutSection = () => {
 		<section className='text-white' id='about'>
 			<div className='md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
 				<Image
-					src='./images/about-img.jpg'
+					src='/images/about-img.jpg'
 					alt='about image'
 					width={500}
 					height={500}

6eb225d71b0a4a7663ed1bc0b0996f60385afb61
rajan <73783957+RK-41@users.noreply.github.com>
2024-08-12 01:39:51 +0530

    Minor tweak

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -2,39 +2,49 @@ import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 export async function POST(req) {
-	if (req.method === 'POST') {
-		try {
-			const { name, email, message } = await req.json();
+	try {
+		// Parsing the incoming JSON data
+		const { name, email, message } = await req.json();
 
-			const transporter = nodemailer.createTransport({
-				host: 'smtp.gmail.com',
-				port: 465,
-				secure: true,
-				auth: {
-					user: 'goforsomethingnew@gmail.com',
-					pass: 'djdx ojtl vume fnzh',
-				},
-			});
+		// Nodemailer transporter
+		const transporter = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: 'goforsomethingnew@gmail.com',
+				pass: 'djdx ojtl vume fnzh',
+			},
+		});
 
-			const mailOptions = {
-				from: email,
-				to: 'goforsomethingnew@gmail.com',
-				subject: 'Contact from portfolio',
-				html: `
-               <h1>Contact Form</h1>
-               <p>Name: ${name}</p>
-               <p>Email: ${email}</p>
-               <p>Message: ${message}</p>
-            `,
-			};
+		// Email options
+		const mailOptions = {
+			from: email,
+			to: 'goforsomethingnew@gmail.com',
+			subject: 'Contact from portfolio',
+			html: `
+				<h1>Contact Form</h1>
+				<p><strong>Name:</strong> ${name}</p>
+				<p><strong>Email:</strong> ${email}</p>
+				<p><strong>Message:</strong> ${message}</p>
+			`,
+		};
 
-			await transporter.sendMail(mailOptions);
+		// Sending the email
+		await transporter.sendMail(mailOptions);
 
-			return NextResponse.json('Email has been sent successfully');
-		} catch {
-			return NextResponse.json('Failed to send the email');
-		}
-	} else {
-		return NextResponse.json('Method not allowed');
+		// Responding with a success message
+		return NextResponse.json(
+			{ message: 'Email has been sent successfully' },
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.error('Failed to send the email:', error);
+
+		// Responding with an error message
+		return NextResponse.json(
+			{ message: 'Failed to send the email' },
+			{ status: 500 }
+		);
 	}
 }

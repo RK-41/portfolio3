5e70d48e19760355b3ab40fce5e0bfa1b3968910
rajan <73783957+RK-41@users.noreply.github.com>
2024-08-12 01:26:18 +0530

    Udated project details button style

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -11,11 +11,13 @@ const ProjectSection = () => {
 			{/* Project slider */}
 			<ProjectSlider />
 
-			<Link href='/projects'>
-				<button className='bg-blue-600 hover:bg-blue-700 font-medium py-2.5 rounded-lg w-full'>
-					Project Details
-				</button>
-			</Link>
+			<div className='w-full flex justify-around'>
+				<Link href='/projects'>
+					<button className='bg-blue-600 hover:bg-blue-700 font-medium py-2.5 rounded-lg w-full px-10 sm:px-0 sm:w-60 md:w-80'>
+						Project Details
+					</button>
+				</Link>
+			</div>
 		</section>
 	);
 };
